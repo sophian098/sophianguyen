530c5cd20287df4d8450c305450d3a7266dbacf4
sophian098 <sophianguyen416@gmail.com>
2026-04-23 06:30:13 -0400

    Update experiences

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,8 +1,23 @@
+import LTS from "../assets/images/LTS.png";
+import Peekapak from "../assets/images/Peekapak.png";
+import UHNCertificate from "../assets/images/UHNCertificate.png";
 import clc from "../assets/images/clc.jpeg";
 import cdl from "../assets/images/cdl.jpeg";
 import robotics from "../assets/images/robotics.jpeg";
 
 export const experiences = [
+    {
+        title: "Software Engineering Intern",
+        image: LTS,
+        subtitle: "Developed digital twin software for real-world system modeling",
+        link: "https://longtermtec.com/"
+    },
+    {
+        title: "Software QA Analyst Intern",
+        image: Peekapak,
+        subtitle: "Tested features for an EdTech platform reaching 800K+ students and educators",
+        link: "https://peekapak.com/"
+    },
     {
         title: "Canada Learning Code Teen Ambassador",
         image: clc,
@@ -14,6 +29,12 @@ export const experiences = [
         image: cdl,
         subtitle: "Shadowed mentors guiding start-ups and participated in emerging technology modules",
         link: "https://creativedestructionlab.com/program/apprentice/"
+    },
+     {
+        title: "UHN STEM Pathways Student",
+        image: UHNCertificate,
+        subtitle: "Explored healthcare innovation through UHN lab visits and virtual STEM talks",
+        link: "https://uhnstempathways.ca/"
     },
     {
         title: "Robotics Club President ",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Socials from "../components/Socials";
 const Hero = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-[60vh] text-center pt-40">
-      {/* Typing Animation for "Hi..." and constant "I'm" */}
+      {/* Typing Animation */}
       <h1 className="text-4xl sm:text-5xl text-gray-600 leading-relaxed mb-3">
         <ReactTyped 
           strings={["Hi", "Bonjour", "Hola", "你好", "こんにちは", "안녕하세요", "Hallo", "Ciao", "Привет", "مرحبا"]}
@@ -24,7 +24,7 @@ const Hero = () => {
 
       {/* Subtitle */}
       <p className="text-2xl text-gray-600 leading-relaxed mb-2">
-        First Year Software Engineering Student at University of Waterloo
+        Software Engineering Student at the University of Waterloo
       </p>
     </div>
   );
